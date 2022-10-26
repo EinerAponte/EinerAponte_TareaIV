8d31b7a4da5020857c36c4af63dc2ff803f64fc5
EinerAponte <efapontec@gmail.com>
2022-10-25 22:43:27 -0500

    Se crea el proyecto y se realizan las respectivas maquetaciones

---
+++ app/src/main/java/com/trabajo/individualsoftware/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.trabajo.individualsoftware
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
